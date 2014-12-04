8f39c0de18dc4d996d1ed7342e41ae74481e4e16
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2014-12-04 14:38:17 -0500

    (6) Add mellanox services

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -1,17 +1,12 @@
 include_recipe "openstack-common"
 include_recipe "openstack-mellanox"
 
-# TODO openstack-neutron-mellanox is not in the repository mlnx-icehouse
-['eswitchd', 'mlnxvif', 'openstack-neutron-mellanox'].each do |pkg|
-  package pkg do
-    action :install
-    provider Chef::Provider::Package::Yum
-  end
+%w[eswitchd mlnxvif openstack-neutron-mellanox].each do |pkg|
+  package pkg
 end
 
 service 'neutron-openvswitch-agent' do
   action [:stop, :disable]
-  #before Service['eswitchd']  # disable this before the eswitchd is started.
 end
 
 service 'eswitchd' do
@@ -27,4 +22,3 @@ end
 service 'openstack-nova-compute' do
   action [:start, :enable]
 end 
-
