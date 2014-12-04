21678165e981c406a8960b0853521c84f56581b3
Rahul <gopinath@eecs.oregonstate.edu>
2014-12-04 14:22:12 -0800

    #9 Add erb files and params

---
+++ attributes/default.rb
@@ -0,0 +1,15 @@
+default['cookbook-openstack-mellanox']['network_mode'] = 'ib'
+default['cookbook-openstack-mellanox']['virt_mode'] = 'pv'
+default['cookbook-openstack-mellanox']['min_vlan'] = '2'
+default['cookbook-openstack-mellanox']['max_vlan'] = '10'
+
+
+default['cookbook-openstack-mellanox']['cinder_inband_ip'] = '1.2.3.4'
+default['cookbook-openstack-mellanox']['mysql_inband_ip'] = '1.2.3.5'
+default['cookbook-openstack-mellanox']['controller_outband'] = '10.20.30.40'
+
+default['cookbook-openstack-mellanox']['network_mode'] = 'eth'
+
+default['cookbook-openstack-mellanox']['mlnx_neutron_root_password'] = 'password'
+
+default['cookbook-openstack-mellanox']['use_namespaces'] = 'false'
